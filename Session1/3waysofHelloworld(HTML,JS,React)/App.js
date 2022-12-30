67a1cde29b6c43952c47562c5e599ff8e7971305
Sabari <sabarihiswaran555@gmail.com>
2022-12-30 12:30:31 +0530

    hello-world Completed

---
+++ Session1/3waysofHelloworld(HTML,JS,React)/App.js
@@ -0,0 +1,20 @@
+const heading1 = React.createElement(
+    "h1",
+    { id: "heading1" },
+    "Heading 1"
+  );
+
+  const heading2 = React.createElement(
+    "h2",
+    { id: "heading2" },
+    "Heading 2"
+  );
+
+  const heading = React.createElement("div", { id: "div1" }, [
+    heading1,
+    heading2,
+  ]);
+
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+
+  root.render(heading);
