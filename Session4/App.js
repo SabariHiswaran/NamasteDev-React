ad874d651530f8045470cdcecf9f6d9570fe2aaf
Sabari <sabarihiswaran555@gmail.com>
2023-01-10 13:10:09 +0530

    Clean-Configuration

--- Session4/App.js
+++ Session4/App.js
@@ -1,1 +1,18 @@
-console.log("Session4")
+import React from "react"
+import  ReactDOM  from "react-dom/client"
+
+const Page = ({ name }) => (  //For multiline JSX we can use parenthesis ()
+   
+<div className="headingdiv">
+  
+    <h1> Heading1</h1>
+    <h2> Heading2</h2>
+    <h3> {name} </h3>
+   
+</div>
+)
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(<Page name="Sabari" />)
