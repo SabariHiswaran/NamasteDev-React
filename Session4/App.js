4bd748367cb1b039a7be3c2c673b1ad5c1409aea
Sabari <sabarihiswaran555@gmail.com>
2023-01-10 17:03:02 +0530

    Food-App-Started

--- Session4/App.js
+++ Session4/App.js
@@ -1,18 +1,10 @@
 import React from "react"
 import  ReactDOM  from "react-dom/client"
 
-const Page = ({ name }) => (  //For multiline JSX we can use parenthesis ()
-   
-<div className="headingdiv">
-  
-    <h1> Heading1</h1>
-    <h2> Heading2</h2>
-    <h3> {name} </h3>
-   
-</div>
+const Title = (
+    <h1 className="title"> Food Mania </h1>
 )
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(<Page name="Sabari" />)
+root.render(Title)
